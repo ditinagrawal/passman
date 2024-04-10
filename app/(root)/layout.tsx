a1a4afd1cbe26e5e7edba2654154bdb2805cd38d
ditinagrawal <ditin.agrawal05@gmail.com>
2024-04-10 11:25:00 +0530

    Added Footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
 
@@ -6,6 +7,7 @@ const Rootlayout = ({ children }: { children: React.ReactNode }) => {
         <div className="w-[90%] mx-auto">
             <Header />
             {children}
+            <Footer />
         </div>
     )
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='font-mono text-center fixed w-[90%] mx-auto bottom-0'>
+            Made with ❤️ by <span className='font-bold'>Ditin Agrawal</span>
+        </footer>
+    )
+}
+
+export default Footer
